384b89b0fd9864cbd0876b152d39719451ab57a4
QASchoolUSA <64873838+QASchoolUSA@users.noreply.github.com>
2021-12-01 00:16:53 -0500

    Added new tests

--- tests/trello.js
+++ tests/trello.js
@@ -1,10 +1,52 @@
 import { Selector } from 'testcafe';
 
-fixture `Trello Web Automation`
+fixture `Testing the Trello Web Applications`
     .page `https://trello-demo-app.vercel.app`;
 
-test('Test1', async t => {
-    const newBoard = Selector('a.navbar-brand');
-    await t
-        .expect(newBoard.innerText).eql(`New Board`);
+
+  test('The user is not able to create a board without providing a name', async t => {
+
+        // Expect to see New Board button, if displayed, click on the button
+        // const newBoard = Selector('a.navbar-brand');
+        await t
+            .expect(Selector('a.navbar-brand').innerText).eql('New Board');
+        await t
+            .click(Selector('a.navbar-brand'))
+
+        // var boardName = Selector('#boardName');
+        // const addBoardButton = Selector('.btn.btn-success');
+
+        await t
+            .expect(Selector('#boardName').value).eql('')
+        await t
+            .expect(Selector('.btn.btn-success').hasAttribute('disabled')).ok();
+
+    });
+
+test('The user is able to name the Trello board', async t => {
+
+      await t
+          .expect(Selector('a.navbar-brand').innerText).eql(`New Board`);
+
+      await t
+          .click(Selector('a.navbar-brand'))
+
+      await t
+          .typeText(Selector('#boardName'), 'Board 1')
+          .expect(Selector('#boardName').value).contains('Board 1')
+
+});
+
+test('The user is able to add the board with a given name', async t => {
+      await t
+        .click(Selector('a.navbar-brand'))
+        .typeText(Selector('#boardName'), 'Board 1')
+        .expect(Selector('#boardName').value).contains('Board 1')
+        .pressKey('enter')
+
+      await t
+          .pressKey('enter')
+
+      await t
+          .expect(Selector('.dropdown a.navbar-brand').innerText).eql(`Board 1`);
 });
