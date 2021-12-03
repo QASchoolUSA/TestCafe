e6ae62bc9bbe4856f0b05a74699b03f4a3f71ab4
QASchoolUSA <qaschoolusa@gmail.com>
2021-12-03 01:04:49 -0500

    Added new changes

--- tests/addItemAndTasks.js
+++ tests/addItemAndTasks.js
@@ -5,19 +5,21 @@ fixture `Testing the Trello Web Applications`
 
 
     test('User should be able to click on new item and new item component is displayed', async t => {
-          await t
+        await t
             .click(Selector('a.navbar-brand'))
             .typeText(Selector('#boardName'), 'Board 1')
             .expect(Selector('#boardName').value).contains('Board 1')
             .click(Selector('.btn.btn-success'))
 
-          await t
-              .expect(Selector('.dropdown a.navbar-brand').innerText).eql(`Board 1`)
+        await t
+            .expect(Selector('.dropdown a.navbar-brand').innerText).eql(`Board 1`)
 
-          await t
+        await t
               .expect(Selector('.btn.btn-success').withText('New Item'))
+
+        await t
               .click(Selector('.btn.btn-success'))
 
-          await t
+        await t
               .expect(Selector('div.panel').visible).ok()
     });
